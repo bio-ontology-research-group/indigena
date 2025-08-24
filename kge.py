0078a860d0d190bb021ea8dc381e282ab3839c55
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2025-08-24 10:29:07 +0300

    fix: Remove .detach() from entity_representations in process_disease

--- kge.py
+++ kge.py
@@ -206,7 +206,7 @@ def main(fold, graph1, graph2, graph3, graph4, model_name, only_test):
     with get_context("spawn").Pool(14) as pool:
         results = []
         with tqdm(total=len(test_pairs), desc='Evaluating test diseases') as pbar:
-            for output in pool.imap_unordered(partial(process_disease, disease2pheno, gene2pheno, gene_pheno_vectors, entity_representations.detach(), entity_to_id), test_pairs, chunksize=10):
+            for output in pool.imap_unordered(partial(process_disease, disease2pheno, gene2pheno, gene_pheno_vectors, entity_representations, entity_to_id), test_pairs, chunksize=10):
                 results.append(output)
                 pbar.update()
 
