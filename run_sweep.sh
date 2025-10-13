b3240b39120adceee3ad14498397dad521acc6fa
Fernando Zhapa Camacho <fernando.zhapacamacho@kaust.edu.sa>
2025-10-13 10:30:55 +0300

    added sweep yml files

---
+++ run_sweep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH -J indigena
+#SBATCH -o out/indigena.%J.out
+#SBATCH -e err/indigena.%J.err
+#SBATCH --mail-user=fernando.zhapacamacho@kaust.edu.sa
+#SBATCH --mail-type=ALL
+#SBATCH --time=6:00:00
+#SBATCH --mem=100G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=[v100]
+
+
+wandb agent --count 1 ferzcam/indigena/$1
